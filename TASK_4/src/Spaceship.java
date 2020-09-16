8aa0487f39e3b377dbb266c1bd5c48be7347a83e
Michael <mholodnuk@gmail.com>
2020-09-16 09:45:34 +0300

    TASK_4

---
+++ TASK_4/src/Spaceship.java
@@ -0,0 +1,75 @@
+public class Spaceship {
+    private String name;
+    private int speed;
+    private static Spaceship[] arrShips = new Spaceship[0];
+    
+    
+    public Spaceship(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+        Spaceship[] sh = new Spaceship[arrShips.length + 1];
+        for(int i = 0; i < arrShips.length; i++){
+            sh[i] = arrShips[i];
+        };
+        sh[sh.length - 1] = this;
+        arrShips = sh;
+    }
+    
+    public static int getAvgSpeed(){
+        /**
+         * Средняя скорость корабля
+         */
+        int avg = 0;
+        int surplus = 0;
+        for (Spaceship arrShip : arrShips) {
+            avg = avg + arrShip.speed / arrShips.length;
+            surplus = surplus + arrShip.speed % arrShips.length;
+        }
+        return avg + surplus/arrShips.length;
+    }
+    
+    public static int getMinSpeed(){
+        /**
+         * Минимальная скорость корабля
+         */
+        if(arrShips.length == 0){return 0;}
+        int min = arrShips[0].speed;
+        for(int i = 1; i < arrShips.length; i++){
+            if(arrShips[i].speed >= min){
+            } else {
+                min = arrShips[i].speed;
+            }
+        }
+        return min;
+    }
+    
+    public static void loadFlot(Spaceship... shipList){
+        /**
+         * Загрузка флота
+         */
+        System.out.println("Flot loaded!");
+    }
+       
+    public int getSpeed() {
+        return speed;
+    }
+    public int getAvgSpeed(Spaceship sc){
+        return this.speed/2 + sc.speed/2;
+    }
+    
+    public int getMinSpeed(Spaceship sc){
+        /**
+         * Минимальная скорость среди 2 кораблей
+         */
+        if(this.speed < sc.speed){
+            return this.speed;
+        }else{
+            return sc.speed;
+        }
+    }
+    
+    @Override
+    public String toString(){
+        return this.name + " speed: " + this.speed;
+    }
+}
---
+++ TASK_4/src/TASK_4.java
@@ -0,0 +1,32 @@
+/*
+ * Напишите обобщенный метод для перестановки двух элементов в списке по индексам. Напишите junit-тесты.
+ */
+
+public class TASK_4 {
+
+    public static void main(String[] args) {
+        Spaceship[] outArray = {new Spaceship("Elizabeth", 100),
+                                new Spaceship("Andrew", 200),
+                                new Spaceship("Броненосец Потемкин", 300),
+                                new Spaceship("Челюскин", 400),
+                                new Spaceship("Andersen", 500),
+                                new Spaceship("Академик Павлов", 600),
+                                new Spaceship("Ледокол Ванюшкин", 700),
+                                new Spaceship("Saint-Andrews", 800),
+                                new Spaceship("Петр I", 900)};
+        swap(5, 9, outArray);
+        for(int k=0; k < outArray.length; System.out.println(outArray[k++]));
+    }
+    
+    
+    public static void swap(int i, int j, Object[] arr){
+        if(i > arr.length || j > arr.length){
+            System.out.println("Index out of bound");
+            return;
+        }
+        
+        Object temp = arr[i-1];
+        arr[i-1] = arr[j-1];
+        arr[j-1] = temp;
+    }
+}
